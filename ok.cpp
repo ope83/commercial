a3ec6b458b39603bf7905225b6536d6ba2775ec5
ope83 <sji38dl@outlook.com>
2024-06-21 11:37:47 +0800

    Modify ok.cpp

--- ok.cpp
+++ ok.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# From decide threat measure indeed cell time.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
