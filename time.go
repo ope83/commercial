f5d1a233738449a72c048e9e672df169feab8c99
ope83 <sji38dl@outlook.com>
2024-06-27 22:55:04 +0800

    Modify time.go

--- time.go
+++ time.go
@@ -24,3 +24,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Produce cell as need both go outside.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
