e48cc2e5a6547e63aaea6f07bb72b2c3e4fed558
ope83 <sji38dl@outlook.com>
2024-06-28 12:47:48 +0800

    Modify loss.cpp

--- loss.cpp
+++ loss.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Arrive compare bar month pressure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
