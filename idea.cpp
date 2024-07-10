5ee73faeb88b8d612315e143bd3f8effd3c7eabc
ope83 <sji38dl@outlook.com>
2024-07-10 12:44:16 +0800

    Modify idea.cpp

--- idea.cpp
+++ idea.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Food forward maybe standard only agree part.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
