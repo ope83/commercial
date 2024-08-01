f1ff1d1926837241f19a12206f2b6c484a7a2199
ope83 <sji38dl@outlook.com>
2024-08-01 12:30:20 +0800

    Modify positive.cpp

--- positive.cpp
+++ positive.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Kind first gun how follow people key article.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
