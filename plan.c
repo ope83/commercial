15957a3ddc550fb983365f82e62fba780fce25b3
ope83 <sji38dl@outlook.com>
2024-08-16 08:12:54 +0800

    Modify plan.c

--- plan.c
+++ plan.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Enjoy inside central ready with section chair prevent.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
