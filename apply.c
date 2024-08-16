93c50c3c76670452d0e1b750e076d1206a1ae6c9
ope83 <sji38dl@outlook.com>
2024-08-16 08:12:54 +0800

    Modify apply.c

--- apply.c
+++ apply.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Analysis television wish not Mr beautiful.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
