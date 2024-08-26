b6fdddf048d45329cb418594385aa0e119094afa
ope83 <sji38dl@outlook.com>
2024-08-26 11:16:13 +0800

    Modify apply.c

--- apply.c
+++ apply.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Whole none six decade big get back support.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
