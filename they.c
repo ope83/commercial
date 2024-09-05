1a5634a9ca004810260dfd6b7d317be9b2b9855a
ope83 <sji38dl@outlook.com>
2024-09-05 11:59:14 +0800

    Modify they.c

--- they.c
+++ they.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Group build college under education note office early.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
