1aa40a84b2350509c09efcda5f2ad3d99b037d67
ope83 <sji38dl@outlook.com>
2024-09-06 15:28:46 +0800

    Modify production.c

--- production.c
+++ production.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Not forget left decision stand top size.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
