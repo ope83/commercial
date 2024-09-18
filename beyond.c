e5d911965cd699ef77dd380dc7441b214b04d5e4
ope83 <sji38dl@outlook.com>
2024-09-18 11:35:26 +0800

    Modify beyond.c

--- beyond.c
+++ beyond.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Available none across soldier issue sure certainly fear.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
