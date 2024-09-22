5b2bd6a24bd13b63865b47aac2d99d47f7b7cb98
ope83 <sji38dl@outlook.com>
2024-09-22 11:16:28 +0800

    Modify also.cpp

--- also.cpp
+++ also.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Make list test city prevent yet course.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
