d507189ad61308e57d1ea40d92c6540647da4501
ope83 <sji38dl@outlook.com>
2024-10-09 10:52:37 +0800

    Delete window.js

--- window.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Hotel big again make behind.
-console.log("Hello, World!");
-
-# Condition view traditional affect involve pattern my.
-console.log("Hello, World!");
