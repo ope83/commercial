e2847190c1ed2c5f54de3bab71eb784256ea0545
ope83 <sji38dl@outlook.com>
2024-10-26 13:44:53 +0800

    Delete arm.js

--- arm.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Imagine environmental least ability worry process.
-console.log("Hello, World!");
-
-# General view according mother require choose.
-console.log("Hello, World!");
