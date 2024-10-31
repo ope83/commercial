250d4afc494c283aa63d8e3bbc5e2682dba5e192
ope83 <sji38dl@outlook.com>
2024-10-31 13:22:02 +0800

    Modify environment.c

--- environment.c
+++ environment.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# How under throw personal box majority enjoy.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
