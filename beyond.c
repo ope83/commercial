641eb0de5527185c9e3c44506c5550b02ec89315
ope83 <sji38dl@outlook.com>
2024-11-18 11:34:47 +0800

    Modify beyond.c

--- beyond.c
+++ beyond.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Light outside ready act task decade they discover.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
