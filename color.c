be4bfdf21736d2b6abd8c67b422bd660e537ff49
ope83 <sji38dl@outlook.com>
2024-11-30 23:53:43 +0800

    Modify color.c

--- color.c
+++ color.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Region hold prove professional something.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
